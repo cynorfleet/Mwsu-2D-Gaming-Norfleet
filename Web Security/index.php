e3bfcf0c3149ce02a386bc227aa0afdee12e90fe
Christian Norfleet <cynorfleet@gmail.com>
2016-07-18 22:20:22 -0500

    sdfsd

--- Web Security/index.php
+++ Web Security/index.php
@@ -28,21 +28,19 @@
     <?php 
      if( !isset ($_POST['stars']))
     {
-         echo "Please enter value";
+         echo "Please choose a value";
      }
     if( isset ($_POST['stars']))
     {
         $answer = $_POST["stars"];
         echo "You rated Dr Passos with: ";
         echo $answer;
-        echo " stars";
-        echo nl2br nl2br; 
-        if($answer <3){
-            <html>
-        <img src="http://img.memecdn.com/bugs-bunny-amp-quot-don-amp-039-t-care-amp-quot_fb_1991675.jpg">;
-            </html>
+        echo " stars"; 
+        if($answer <3)
+        {
+             $path = './'.$dir.'http://img.memecdn.com/bugs-bunny-amp-quot-don-amp-039-t-care-amp-quot_fb_1991675.jpg';
+            print '<img src="'.$path.'" alt="dank meme" />';
         }
-        
     }
     ?>
         <br>
