18ee79ee31e431f9151bc750e6c669d179281df8
Christian Norfleet <cynorfleet@gmail.com>
2016-07-18 22:56:02 -0500

    moved radio over to left

--- Web Security/index.php
+++ Web Security/index.php
@@ -7,21 +7,18 @@
 <body>
     How would you rate Dr. Passos?
     <form name="rate" method="POST">
-
-        <div align="center">
-            <br>
-            <input type="radio" name="stars" value="1"> 1 Star
-            <br>
-            <input type="radio" name="stars" value="2"> 2 Stars
-            <br>
-            <input type="radio" name="stars" value="3"> 3 Stars
-            <br>
-            <input type="radio" name="stars" value="4"> 4 Stars
-            <br>
-            <input type="radio" name="stars" value="5"> 5 Stars
-            <br>
-            <input type="submit" value="submit">
-        </div>
+        <br>
+        <input type="radio" name="stars" value="1"> 1 Star
+        <br>
+        <input type="radio" name="stars" value="2"> 2 Stars
+        <br>
+        <input type="radio" name="stars" value="3"> 3 Stars
+        <br>
+        <input type="radio" name="stars" value="4"> 4 Stars
+        <br>
+        <input type="radio" name="stars" value="5"> 5 Stars
+        <br>
+        <input type="submit" value="submit">
     </form>
     <br>
     <br>
