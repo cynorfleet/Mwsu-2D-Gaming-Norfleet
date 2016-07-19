07d651df170cb0825d1fd7dfdcd2a33ff3933d37
Christian Norfleet <cynorfleet@gmail.com>
2016-07-18 22:47:35 -0500

    swag

--- Web Security/index.php
+++ Web Security/index.php
@@ -40,9 +40,20 @@
         echo  nl2br ("\n");
         echo  nl2br ("\n");
 
-        if($answer <3)
+        if($answer <2)
         {
              $path = 'http://img.memecdn.com/bugs-bunny-amp-quot-don-amp-039-t-care-amp-quot_fb_1991675.jpg';
+            print '<img src="'.$path.'" alt="hater" />';
+        }
+        
+        if($answer <4)
+        {
+             $path = 'http://m.memegen.com/obe54d.jpg';
+            print '<img src="'.$path.'" alt="damn son" />';
+        }
+        if($answer <=5)
+        {
+             $path = '/SWAG.png';
             print '<img src="'.$path.'" alt="dank meme" />';
         }
     }
