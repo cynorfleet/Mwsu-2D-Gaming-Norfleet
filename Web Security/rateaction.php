c1c25727bb9a210f7b6151ab0ba61c1e53362f77
Christian Norfleet <cynorfleet@gmail.com>
2016-07-18 21:18:30 -0500

    added some pics

--- Web Security/rateaction.php
+++ Web Security/rateaction.php
@@ -8,6 +8,27 @@
     <?php echo $_POST["stars"]; ?>
         stars
         <br>
+        <br>
+        <?php
+    
+    $rated = $_POST["stars"];
+    
+        if ($rated < 3)
+        {
+                <img src="pic_mountain.jpg" alt="Mountain View" style="width:304px;height:228px;">
+        }
+    
+        elseif ($rated < 4)
+        {
+            <img src="pic_mountain.jpg" alt="Mountain View" style="width:304px;height:228px;">
+        }
+    
+        else
+        {
+                <img src="pic_mountain.jpg" alt="Mountain View" style="width:304px;height:228px;">
+        }
+        
+    ?>
 
 
 </body>
